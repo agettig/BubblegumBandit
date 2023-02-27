0eacd5883c306231ec8c60a16cc320eb491776ac
Teddy Siker <92184485+sikernisky@users.noreply.github.com>
2023-02-27 16:00:28 -0500

    added bubblegum

--- JSONDemo/core/src/edu/cornell/gdiac/json/gum/Bubblegum.java
+++ JSONDemo/core/src/edu/cornell/gdiac/json/gum/Bubblegum.java
@@ -1,6 +1,41 @@
 package edu.cornell.gdiac.json.gum;
 
+import com.badlogic.gdx.math.Vector2;
+import edu.cornell.gdiac.physics.obstacle.WheelObstacle;
+
 public class Bubblegum {
+
+    /**Diameter of a Bubblegum projectile */
+    private float diameter;
+
+    /**X-coordinate of Bubblegum projectile */
+    public float x;
+
+    /**Y-coordinate of Bubblegum projectile */
+    public float y;
+
+    /**X-coordinate of Bubblegum velocity */
+    public float vx;
+
+    /**Y-coordinate of Bubblegum velocity */
+    public float vy;
+
+    /**
+     * Creates a Bubblegum projectile.
+     * */
+    public Bubblegum(){
+
+    }
+
+
+    /**
+     * Returns the (X,Y) position of this Bubblegum projectile.
+     *
+     * @returns a Vector2 representing this Bubblegum's screen position
+     * */
+    public Vector2 getPosition(){
+        return new Vector2(x, y);
+    }
 
 
 }
