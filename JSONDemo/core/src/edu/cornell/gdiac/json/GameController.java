1981c4285ad9ec92985b5d6a43462bb8ab313538
Ben Neuwirth <bmn36@cornell.edu>
2023-02-28 16:26:34 -0500

    Restore directional aiming
    - I accidentally overwrote this before

--- JSONDemo/core/src/edu/cornell/gdiac/json/GameController.java
+++ JSONDemo/core/src/edu/cornell/gdiac/json/GameController.java
@@ -16,6 +16,7 @@
 package edu.cornell.gdiac.json;
 
 import com.badlogic.gdx.*;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.*;
 import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.*;
@@ -69,7 +70,7 @@ public class GameController implements Screen, ContactListener {
 
 	/** Reference to the game level */
 	protected LevelModel level;
-		
+
 	/** Whether or not this is an active controller */
 	private boolean active;
 	/** Whether we have completed this level */
@@ -131,7 +132,7 @@ public class GameController implements Screen, ContactListener {
 		}
 		failed = value;
 	}
-	
+
 	/**
 	 * Returns true if this is the active screen
 	 *
@@ -151,7 +152,7 @@ public class GameController implements Screen, ContactListener {
 	public GameCanvas getCanvas() {
 		return canvas;
 	}
-	
+
 	/**
 	 * Sets the canvas associated with this controller
 	 *
@@ -163,9 +164,9 @@ public class GameController implements Screen, ContactListener {
 	public void setCanvas(GameCanvas canvas) {
 		this.canvas = canvas;
 	}
-	
+
 	/**
-	 * Creates a new game world 
+	 * Creates a new game world
 	 *
 	 * The physics bounds and drawing scale are now stored in the LevelModel and
 	 * defined by the appropriate JSON file.
@@ -181,7 +182,7 @@ public class GameController implements Screen, ContactListener {
 		setFailure(false);
 		sensorFixtures = new ObjectSet<Fixture>();
 	}
-	
+
 	/**
 	 * Dispose of all (non-static) resources allocated to this mode.
 	 */
@@ -210,25 +211,25 @@ public class GameController implements Screen, ContactListener {
 		// This represents the level but does not BUILD it
 		levelFormat = directory.getEntry( "level1", JsonValue.class );
 	}
-	
+
 	/**
 	 * Resets the status of the game so that we can play again.
 	 *
-	 * This method disposes of the level and creates a new one. It will 
+	 * This method disposes of the level and creates a new one. It will
 	 * reread from the JSON file, allowing us to make changes on the fly.
 	 */
 	public void reset() {
 		level.dispose();
-		
+
 		setComplete(false);
 		setFailure(false);
 		countdown = -1;
-		
+
 		// Reload the json each time
 		level.populate(directory, levelFormat);
 		level.getWorld().setContactListener(this);
 	}
-	
+
 	/**
 	 * Returns whether to process the update loop
 	 *
@@ -237,7 +238,7 @@ public class GameController implements Screen, ContactListener {
 	 * normally.
 	 *
 	 * @param dt Number of seconds since last animation frame
-	 * 
+	 *
 	 * @return whether to process the update loop
 	 */
 	public boolean preUpdate(float dt) {
@@ -251,12 +252,12 @@ public class GameController implements Screen, ContactListener {
 		if (input.didDebug()) {
 			level.setDebug(!level.getDebug());
 		}
-		
+
 		// Handle resets
 		if (input.didReset()) {
 			reset();
 		}
-		
+
 		// Now it is time to maybe switch screens.
 		if (input.didExit()) {
 			listener.exitScreen(this, EXIT_QUIT);
@@ -266,15 +267,15 @@ public class GameController implements Screen, ContactListener {
 		} else if (countdown == 0) {
 			reset();
 		}
-		
+
 		if (!isFailure() && level.getAvatar().getY() < -1) {
 			setFailure(true);
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
 	/**
 	 * The core gameplay loop of this world.
 	 *
@@ -288,23 +289,23 @@ public class GameController implements Screen, ContactListener {
 	public void update(float dt) {
 		// Process actions in object model
 		DudeModel avatar = level.getAvatar();
-		avatar.setMovement(InputController.getInstance().getHorizontal() *avatar.getForce());
+		avatar.setMovement(InputController.getInstance().getHorizontal() * avatar.getForce());
 		avatar.setJumping(InputController.getInstance().didPrimary());
-		
 		avatar.applyForce();
 		if (avatar.isJumping()) {
 			jumpId = playSound( jumpSound, jumpId );
 		}
 
 		if (InputController.getInstance().didShoot()) {
-			createGum(); // Fire gum if shoot
+			// TODO: Visible crosshair?
+			createGum(InputController.getInstance().getCrossHair()); // Fire gum if shoot
 		}
 
 		// Turn the physics engine crank.
 		level.getWorld().step(WORLD_STEP,WORLD_VELOC,WORLD_POSIT);
 		level.update(dt);
 	}
-	
+
 	/**
 	 * Draw the physics objects to the canvas
 	 *
@@ -317,9 +318,9 @@ public class GameController implements Screen, ContactListener {
 	 */
 	public void draw(float delta) {
 		canvas.clear();
-		
+
 		level.draw(canvas);
-		
+
 		// Final message
 		if (complete && !failed) {
 			displayFont.setColor(Color.YELLOW);
@@ -333,11 +334,11 @@ public class GameController implements Screen, ContactListener {
 			canvas.end();
 		}
 	}
-	
+
 	/**
-	 * Called when the Screen is resized. 
+	 * Called when the Screen is resized.
 	 *
-	 * This can happen at any point during a non-paused state but will never happen 
+	 * This can happen at any point during a non-paused state but will never happen
 	 * before a call to show().
 	 *
 	 * @param width  The new width in pixels
@@ -366,8 +367,8 @@ public class GameController implements Screen, ContactListener {
 
 	/**
 	 * Called when the Screen is paused.
-	 * 
-	 * This is usually when it's not active or visible on screen. An Application is 
+	 *
+	 * This is usually when it's not active or visible on screen. An Application is
 	 * also paused before it is destroyed.
 	 */
 	public void pause() {
@@ -385,7 +386,7 @@ public class GameController implements Screen, ContactListener {
 	public void resume() {
 		// TODO Auto-generated method stub
 	}
-	
+
 	/**
 	 * Called when this screen becomes the current screen for a Game.
 	 */
@@ -414,7 +415,7 @@ public class GameController implements Screen, ContactListener {
 	/**
 	 * Callback method for the start of a collision
 	 *
-	 * This method is called when we first get a collision between two objects.  We use 
+	 * This method is called when we first get a collision between two objects.  We use
 	 * this method to test if it is the "right" kind of collision.  In particular, we
 	 * use it to test if we made it to the win door.
 	 *
@@ -429,21 +430,21 @@ public class GameController implements Screen, ContactListener {
 
 		Object fd1 = fix1.getUserData();
 		Object fd2 = fix2.getUserData();
-		
+
 		try {
 			Obstacle bd1 = (Obstacle)body1.getUserData();
 			Obstacle bd2 = (Obstacle)body2.getUserData();
 
 			DudeModel avatar = level.getAvatar();
 			BoxObstacle door = level.getExit();
-			
+
 			// See if we have landed on the ground.
 			if ((avatar.getSensorName().equals(fd2) && avatar != bd1) ||
 				(avatar.getSensorName().equals(fd1) && avatar != bd2)) {
 				avatar.setGrounded(true);
 				sensorFixtures.add(avatar == bd1 ? fix2 : fix1); // Could have more than one ground
 			}
-			
+
 			// Check for win condition
 			if ((bd1 == avatar && bd2 == door  ) ||
 				(bd1 == door   && bd2 == avatar)) {
@@ -470,7 +471,7 @@ public class GameController implements Screen, ContactListener {
 	 * This method is called when two objects cease to touch.  The main use of this method
 	 * is to determine when the characer is NOT on the ground.  This is how we prevent
 	 * double jumping.
-	 */ 
+	 */
 	public void endContact(Contact contact) {
 		Fixture fix1 = contact.getFixtureA();
 		Fixture fix2 = contact.getFixtureB();
@@ -480,7 +481,7 @@ public class GameController implements Screen, ContactListener {
 
 		Object fd1 = fix1.getUserData();
 		Object fd2 = fix2.getUserData();
-		
+
 		Object bd1 = body1.getUserData();
 		Object bd2 = body2.getUserData();
 
@@ -493,7 +494,7 @@ public class GameController implements Screen, ContactListener {
 			}
 		}
 	}
-	
+
 	/** Unused ContactListener method */
 	public void postSolve(Contact contact, ContactImpulse impulse) {}
 	/** Unused ContactListener method */
@@ -542,16 +543,21 @@ public class GameController implements Screen, ContactListener {
 	/**
 	 * Add a new bullet to the world and send it in the right direction.
 	 */
-	private void createGum() {
+	private void createGum(Vector2 target) {
 		JsonValue gumJV = levelFormat.get("gum");
 		DudeModel avatar = level.getAvatar();
 		float offset = gumJV.getFloat("offset",0);
-		offset *= (avatar.isFacingRight() ? 1 : -1);
+		offset *= (target.x > avatar.getX() ? 1 : -1);
+		float startX = avatar.getX() + offset;
+		float startY = avatar.getY();
+
 		String key = gumJV.get("texture").asString();
 		TextureRegion gumTexture = new TextureRegion(directory.getEntry(key, Texture.class));
 		float radius = gumTexture.getRegionWidth()/(2.0f*level.getScale().x);
-		WheelObstacle gum = new WheelObstacle(avatar.getX()+offset, avatar.getY(), radius);
 
+		WheelObstacle gum = new WheelObstacle(startX, startY, radius);
+
+		// Physics properties
 		gum.setName(gumJV.name());
 		gum.setDensity(gumJV.getFloat("density", 0));
 		gum.setDrawScale(level.getScale());
@@ -562,8 +568,11 @@ public class GameController implements Screen, ContactListener {
 
 		// Compute position and velocity
 		float speed = gumJV.getFloat( "speed", 0 );
-		speed  *= (avatar.isFacingRight() ? 1 : -1);
-		gum.setVX(speed);
+		Vector2 gumVel = new Vector2(target.x - startX, target.y - startY);
+		gumVel.nor();
+		gumVel.scl(speed);
+		gum.setVX(gumVel.x);
+		gum.setVY(gumVel.y);
 		level.activate(gum);
 	}
 
