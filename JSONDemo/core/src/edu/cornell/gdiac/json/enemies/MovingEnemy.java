343edf261208fd83ad33cd7f39e0125106e7f0e2
Ruike Liang <rl582@cornell.edu>
2023-03-05 16:45:02 -0500

    basic enemy movement

--- JSONDemo/core/src/edu/cornell/gdiac/json/enemies/MovingEnemy.java
+++ JSONDemo/core/src/edu/cornell/gdiac/json/enemies/MovingEnemy.java
@@ -1,12 +1,15 @@
 package edu.cornell.gdiac.json.enemies;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.JsonValue;
 import edu.cornell.gdiac.assets.AssetDirectory;
+import edu.cornell.gdiac.json.DudeModel;
 
-public class MovingEnemy  extends Enemy{
 
-    private enum EnemyState{
+public class MovingEnemy  extends Enemy {
+
+    private enum EnemyState {
         /**
          * The enemy is stationary
          */
@@ -26,26 +29,39 @@ public class MovingEnemy  extends Enemy{
         ATTACK
     }
 
+    /** The radius of how far in front of them the robot can see*/
     private float visionRadius;
 
+    /** How far the robot is allowed to grab*/
+    private float grabRadius;
+    /** Time passed since the controller was started*/
+    private long ticks;
+    /** State of the robot*/
     private EnemyState state;
+    /**Represents the player aka Bubblegum Bandit*/
+    private DudeModel target;
 
     public void setVisionRadius(float visionRadius) {
         this.visionRadius = visionRadius;
     }
 
-    public void setEnemyState(EnemyState state){
+    public void setGrabRadius(float grabRadius) {this.grabRadius = grabRadius;}
+
+    public void setEnemyState(EnemyState state) {
         this.state = state;
     }
 
-    public MovingEnemy(){
-        super(0,0,1,1);
+    public MovingEnemy() {
+        super(0, 0, 1, 1);
     }
 
-    public void initialize(AssetDirectory directory, JsonValue json){
+    public void initialize(AssetDirectory directory, JsonValue json, DudeModel target) {
         super.initialize(directory, json);
-        setVisionRadius(json.get("visionradius").asFloat());
+        setVisionRadius(json.get("vision\n" +
+                "    public void setVisionRadius(float visionradius").asFloat());
+        setGrabRadius(json.get("grabradius").asFloat());
         setEnemyState(EnemyState.valueOf(json.get("enemystate").asString()));
+        this.target = target;
     }
 
     // TODO
@@ -60,29 +76,47 @@ public class MovingEnemy  extends Enemy{
         applyForce();
     }
 
+    // TODO checkGrabRange
+    public boolean checkGrabRange(Vector2 player, Vector2 robot) {
+        return false;
+    }
+
+    // TODO checkVisionRange
+    public boolean checkVisionRange(Vector2 player, Vector2 robot) {
+        return false;
+    }
+
     // TODO changeStateIfApplicable
     public void changeStateIfApplicable(){
         // TODO add initialization
 
         switch(state){
             case STATIONARY:
-
+                this.state = EnemyState.WANDER;
                 break;
 
             case WANDER:
-
-
+                if (checkVisionRange(this.target.getPosition(), this.getPosition())) {
+                    this.state = EnemyState.CHASE;
+                }
                 break;
 
             case CHASE:
-
-
+                if (checkGrabRange(this.target.getPosition(), this.getPosition())) {
+                    this.state = EnemyState.ATTACK;
+                }
+                else if (target.getWidth() + ticks % 10 == 0) {
+                    this.state = EnemyState.WANDER;
+                }
                 break;
 
             case ATTACK:
-
-
-
+                if (checkGrabRange(this.target.getPosition(), this.getPosition())) {
+                    this.state = EnemyState.CHASE;
+                }
+                else {
+                    //game over???
+                }
                 break;
 
             default:
