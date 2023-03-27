1d1c7dbeb4e0b2a2d127738b25bd7b691cd8aaba
Ben Neuwirth <bmn36@cornell.edu>
2023-03-27 16:17:17 -0400

    Add support for any layer order in Tiled and fix bugs
    - Enemy raycast only occluded by terrain
    - Tiled layers can be in any order

--- core/src/edu/cornell/gdiac/bubblegumbandit/controllers/CollisionController.java
+++ core/src/edu/cornell/gdiac/bubblegumbandit/controllers/CollisionController.java
@@ -17,11 +17,12 @@ import edu.cornell.gdiac.bubblegumbandit.models.level.LevelModel;
 
 public class CollisionController implements ContactListener {
 
-    public static final short CATEGORY_PLAYER = 0x0001;
     public static final short CATEGORY_ENEMY = 0x0002;
     public static final short CATEGORY_TERRAIN = 0x0004;
     public static final short CATEGORY_GUM = 0x0008;
     public static final short CATEGORY_PROJECTILE = 0x0010;
+    public static final short CATEGORY_PLAYER = 0x0020;
+    public static final short CATEGORY_EVENTTILE = 0x0040;
 
     public static final short MASK_PLAYER = ~CATEGORY_GUM;
     public static final short MASK_ENEMY = ~CATEGORY_ENEMY;
@@ -29,6 +30,7 @@ public class CollisionController implements ContactListener {
     public static final short MASK_GUM = ~(CATEGORY_PLAYER | CATEGORY_GUM);
     public static final short MASK_GUM_LIMIT = ~(CATEGORY_PLAYER | CATEGORY_GUM | CATEGORY_ENEMY);
     public static final short MASK_PROJECTILE = ~(CATEGORY_PROJECTILE | CATEGORY_ENEMY);
+    public static final short MASK_EVENTTILE = CATEGORY_PLAYER;
 
     /** The amount of gum collected when collecting floating gum */
     private static final int AMMO_AMOUNT = 5;
