6ba9ac4606fa5453b17151c8c95ffcedb59f6d51
emily-penna <emilypenna@gmail.com>
2023-04-08 17:34:00 -0400

    sunfish is a wheel obstacle

--- core/src/edu/cornell/gdiac/bubblegumbandit/models/SunfishModel.java
+++ core/src/edu/cornell/gdiac/bubblegumbandit/models/SunfishModel.java
@@ -2,18 +2,24 @@ package edu.cornell.gdiac.bubblegumbandit.models;
 
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import edu.cornell.gdiac.bubblegumbandit.view.GameCanvas;
+import edu.cornell.gdiac.physics.obstacle.WheelObstacle;
 import org.w3c.dom.Text;
 
 /** Class to represent the moving ship on the level select screen */
-public class SunfishModel {
+public class SunfishModel extends WheelObstacle {
 
     /** texture of the sunfish */
     private TextureRegion texture;
-    public SunfishModel (TextureRegion texture){
+
+    private static final float RADIUS = 10f;
+
+    public SunfishModel (TextureRegion texture, float x, float y){
+        super(x, y, RADIUS);
         this.texture = texture;
     }
     public void draw(GameCanvas canvas){
-        canvas.draw(texture, 100, 100);
+        canvas.draw(texture, getX(), getY());
     }
 }
