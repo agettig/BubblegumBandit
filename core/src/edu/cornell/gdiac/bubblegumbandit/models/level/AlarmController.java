b1561d5221b3ced8a892a174217bd2dd9632f71a
Caroline <crh255@cornell.edu>
2023-04-15 14:00:12 -0400

    progress on outlines

--- core/src/edu/cornell/gdiac/bubblegumbandit/models/level/AlarmController.java
+++ core/src/edu/cornell/gdiac/bubblegumbandit/models/level/AlarmController.java
@@ -115,8 +115,8 @@ public class AlarmController {
       if(time>pulseTime/3f&&time<pulseTime) {
 
         for(PointLight light : lights) {
-          light.setDistance(time<= pulseTime*1.5f ? light.getDistance()+.3f//ACTIVE_DIST/150
-              : light.getDistance()-.3f);//ACTIVE_DIST/150);
+          light.setDistance(time<= pulseTime*1.5f ? light.getDistance()+.3f
+              : light.getDistance()-.3f);
         }
 
       } else if (time>pulseTime) { {
