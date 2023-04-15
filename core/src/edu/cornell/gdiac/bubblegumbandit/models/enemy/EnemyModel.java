47cc4a047f8d0ee205a4cba5b2c2b823bee2bb02
Caroline <crh255@cornell.edu>
2023-04-15 16:35:37 -0400

    More exact fix for rolling robot, not perfect execution but results check out.

--- core/src/edu/cornell/gdiac/bubblegumbandit/models/enemy/EnemyModel.java
+++ core/src/edu/cornell/gdiac/bubblegumbandit/models/enemy/EnemyModel.java
@@ -414,16 +414,16 @@ public abstract class EnemyModel extends CapsuleObstacle implements Gummable {
             // TODO: Fix rolling robots so don't have to do this
             float y = getY() * drawScale.y;
             if (getName().equals("rollingrobot")) {
-                y += yScale * 16;
-                x += effect * 40;
+                y += 10*yScale;
             }
+
 
             //if gum, overlay with gumTexture
             if (gummed) {
                 canvas.drawWithShadow(drawn, Color.WHITE, origin.x, origin.y, getX() * drawScale.x,
-                        getY() * drawScale.y, getAngle(), effect, yScale);
+                        y, getAngle(), effect, yScale);
                 canvas.draw(gumTexture, Color.WHITE, origin.x, origin.y, getX() * drawScale.x,
-                        getY() * drawScale.y, getAngle(), 1, yScale);
+                        y, getAngle(), 1, yScale);
             } else {
                 canvas.drawWithShadow(drawn, Color.WHITE, origin.x, origin.y, x,
                         y, getAngle(), effect, yScale);
@@ -435,12 +435,16 @@ public abstract class EnemyModel extends CapsuleObstacle implements Gummable {
     }
     public void drawWithOutline(GameCanvas canvas) {
         if (outline != null && gummedTexture != null) {
+            float y = getY() * drawScale.y;
+            if (getName().equals("rollingrobot")) {
+                y += 10*yScale;
+            }
             float effect = faceRight ? 1.0f : -1.0f;
             canvas.drawWithShadow(gummedTexture, Color.WHITE, origin.x, origin.y, getX() * drawScale.x,
-                    getY() * drawScale.y, getAngle(), effect, yScale);
+                    y, getAngle(), effect, yScale);
             if (gummed) {
                 canvas.draw(outline, Color.WHITE, origin.x, origin.y, getX()* drawScale.x-5,
-                    getY() * drawScale.y-5*yScale, getAngle(), 1, yScale);
+                    y-5*yScale, getAngle(), 1, yScale);
             }
         }
     }
