7310d3501300e84f14a8ac08c00803ef66c755cb
Caroline <crh255@cornell.edu>
2023-04-17 20:22:25 -0400

    Using constants for state in SaveData

--- core/src/edu/cornell/gdiac/bubblegumbandit/helpers/SaveData.java
+++ core/src/edu/cornell/gdiac/bubblegumbandit/helpers/SaveData.java
@@ -5,35 +5,46 @@ import com.badlogic.gdx.Preferences;
 
 public class SaveData {
 
-  public static int LOCKED = -2;
-  public static int NOT_FOUND = -3;
-  public static int INCOMPLETE = -1;
-  private static String prefsName = "save";
 
+  private static String prefsName = "save";
+  private static int INCOMPLETE = -1;
+  private static int LOCKED = -2;
+  private static int NOT_FOUND = -3;
+  //any numbers above -1 represent the number of stars collected
 
   public static boolean saveExists() {
-
-  }
-  public static void makeSave(int levelCount) {
-
+    return Gdx.app.getPreferences(prefsName).getBoolean("save created", false);
   }
 
+  public static void makeData(int levelCount, boolean lockLevels) {
+    Preferences prefs =  Gdx.app.getPreferences(prefsName);
+    prefs.putInteger("1", -1);
+    for(int i = 2; i<levelCount+1; i++) {
+      prefs.putInteger(""+i, lockLevels? LOCKED : INCOMPLETE);
+    }
+  }
 
-  public static boolean unlocked(int levelName) {
+  private static int getLevelStatus(int level) {
+    int status =  Gdx.app.getPreferences(prefsName).getInteger(""+level, NOT_FOUND);
+    if(status == NOT_FOUND) System.err.println("Could not find level "+level+" when retrieving save data. ");
+    return status;
 
   }
 
-  public static int stars(int level) {
-
+  public static boolean completed(int level) {
+    return getLevelStatus(level)>-1;
   }
 
-  public static boolean levelExists(int level) {
-
+  public static int getStars(int level) {
+    int status = getLevelStatus(level);
+    if(status<0) System.err.println("Requested star count from incomplete or locked level.");
+    return status;
   }
 
-
-
-
+  public static boolean unlocked(int level) {
+    int status = getLevelStatus(level);
+    return status >LOCKED;
+  }
 
   public static void unlock(int level) {
     Preferences prefs = Gdx.app.getPreferences(prefsName);
@@ -56,5 +67,6 @@ public class SaveData {
     prefs.flush();
 
   }
+
 
 }
