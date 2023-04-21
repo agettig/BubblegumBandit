88148839d838f3b964745ff5a2133e262e7cc3bf
emily-penna <emilypenna@gmail.com>
2023-04-21 10:41:29 -0400

    merged with main

---
+++ core/src/edu/cornell/gdiac/bubblegumbandit/helpers/Shield.java
@@ -0,0 +1,12 @@
+package edu.cornell.gdiac.bubblegumbandit.helpers;
+
+public interface Shield {
+
+    public boolean isShielded();
+
+    public void isShielded(boolean value);
+
+    public void setUnshieldedTexture();
+
+    public void setShieldedTexture();
+}
