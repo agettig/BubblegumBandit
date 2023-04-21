43a117c7fa9adee468ac6c503aaae1ddf7688a82
Teddy Siker <92184485+sikernisky@users.noreply.github.com>
2023-04-20 20:29:17 -0400

    drawing bfix

--- core/src/edu/cornell/gdiac/bubblegumbandit/controllers/GameController.java
+++ core/src/edu/cornell/gdiac/bubblegumbandit/controllers/GameController.java
@@ -389,6 +389,9 @@ public class GameController implements Screen {
         stuckGum = new TextureRegion(directory.getEntry("splat_gum", Texture.class));
         hud = new HUDController(directory);
         minimap = new Minimap();
+        int x = levelFormat.get("width").asInt();
+        int y = levelFormat.get("height").asInt();
+        minimap.initialize(directory, levelFormat, x, y);
     }
 
 
@@ -430,9 +433,6 @@ public class GameController implements Screen {
         projectileController.initialize(constantsJson.get("projectile"), directory, level.getScale().x, level.getScale().y);
         collisionController.initialize(canvas.getCamera());
         canvas.getCamera().setLevelSize(level.getBounds().width * level.getScale().x, level.getBounds().height * level.getScale().y);
-        int x = levelFormat.get("width").asInt();
-        int y = levelFormat.get("height").asInt();
-        minimap.initialize(directory, levelFormat, x, y);
     }
 
     /**
