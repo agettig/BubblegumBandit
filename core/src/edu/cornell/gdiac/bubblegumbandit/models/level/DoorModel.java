b130821eb11122fb6ffd97d5c4bde95f52ce10b5
Ben Neuwirth <bmn36@cornell.edu>
2023-04-23 11:50:10 -0400

    Proximity based doors that open when enemies / player is near

--- core/src/edu/cornell/gdiac/bubblegumbandit/models/level/DoorModel.java
+++ core/src/edu/cornell/gdiac/bubblegumbandit/models/level/DoorModel.java
@@ -5,9 +5,17 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.JsonValue;
 
+import com.badlogic.gdx.utils.ObjectSet;
 import edu.cornell.gdiac.assets.AssetDirectory;
+import edu.cornell.gdiac.bubblegumbandit.controllers.CollisionController;
+import edu.cornell.gdiac.bubblegumbandit.view.GameCanvas;
+import edu.cornell.gdiac.physics.obstacle.Obstacle;
 import java.lang.reflect.Field;
 
 public class DoorModel extends TileModel {
@@ -84,6 +92,19 @@ public class DoorModel extends TileModel {
         return secondLowerRight;
     }
 
+    /** Sensor info to detect when someone is in range of a door*/
+    private Fixture sensorFixture;
+    private PolygonShape sensorShape;
+    private Color sensorColor;
+
+    /** Whether the door is open. */
+    private boolean isOpen;
+
+    /** The obstacles in the range of the door. */
+    private final ObjectSet<Obstacle> obsInRange;
+
+    /** The physics world */
+
     /** Constructs a new DoorModel
      * Uses generic values to start
      */
@@ -93,6 +114,8 @@ public class DoorModel extends TileModel {
         firstLowerRight = new Vector2();
         secondUpperLeft = new Vector2();
         secondLowerRight = new Vector2();
+        isOpen = false;
+        obsInRange = new ObjectSet<>();
     }
 
     /** Initializes the camera tile model.
@@ -113,7 +136,7 @@ public class DoorModel extends TileModel {
         float width = objectJson.getFloat("width") / scale.x;
         float height = objectJson.getFloat("height") / scale.y;
         setDimension(width, height);
-        setSensor(true);
+        setSensor(false);
         this.isHorizontal = isHorizontal;
 
         setBodyType(BodyDef.BodyType.StaticBody);
@@ -134,6 +157,24 @@ public class DoorModel extends TileModel {
         String key = constants.get("texture").asString();
         TextureRegion texture = new TextureRegion(directory.getEntry(key, Texture.class));
         setTexture(texture);
+
+        // Initialize the sensors used to detect when things are being crushed.
+        // Get the sensor information
+        Vector2 sensorCenter = new Vector2(0, 0);
+        float[] sSize = constants.get("sensorsize").asFloatArray();
+        sensorShape = new PolygonShape();
+        sensorShape.setAsBox(sSize[0], sSize[1], sensorCenter, 0.0f);
+
+        // Reflection is best way to convert name to color
+        try {
+            String cname = constants.get("sensorcolor").asString().toUpperCase();
+            Field field = Class.forName("com.badlogic.gdx.graphics.Color").getField(cname);
+            sensorColor = new Color((Color) field.get(null));
+        } catch (Exception e) {
+            sensorColor = null; // Not defined
+        }
+        opacity = constants.get("sensoropacity").asInt();
+        sensorColor.mul(opacity / 255.0f);
 
         // Set object data
         isFirstFixedX = false;
@@ -188,4 +229,64 @@ public class DoorModel extends TileModel {
         }
         setDrawScale(scale);
     }
+
+    public boolean activatePhysics(World world) {
+        // create the box from our superclass
+        if (!super.activatePhysics(world)) {
+            return false;
+        }
+
+        FixtureDef sensorDef = new FixtureDef();
+        sensorDef.density = getDensity();
+        sensorDef.isSensor = true;
+        sensorDef.shape = sensorShape;
+        sensorFixture = body.createFixture(sensorDef);
+        sensorFixture.setUserData(this);
+        return true;
+    }
+
+    /**
+     * Adds an obstacle in the range of the door's sensor.
+     * Opens the door.
+     *
+     * @param ob the obstacle in the range of the door*/
+    public void addObInRange(Obstacle ob) {
+        obsInRange.add(ob);
+    }
+
+    /**
+     * Removes an obstacle in the range of the door's sensor.
+     * Closes the door, if there are no obstacles in range.
+     *
+     * @param ob the obstacle no longer in the range of the door*/
+    public void removeObInRange(Obstacle ob) {
+        obsInRange.remove(ob);
+    }
+
+    private void openDoor() {
+        isOpen = true;
+        body.getFixtureList().get(0).setSensor(true);
+    }
+
+    private void closeDoor() {
+        isOpen = false;
+        body.getFixtureList().get(0).setSensor(false);
+    }
+
+    public void update(float dt) {
+        super.update(dt);
+        if (obsInRange.size == 0 && isOpen) {
+            closeDoor();
+        } else if (obsInRange.size > 0 && !isOpen) {
+            openDoor();
+        }
+
+    }
+
+    public void draw(GameCanvas canvas) {
+        if (!isOpen) {
+            super.draw(canvas);
+        }
+    }
+
 }
