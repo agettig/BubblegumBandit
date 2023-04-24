21813e804a1247521ca82f2d671cedd206cf55e7
Ben Neuwirth <bmn36@cornell.edu>
2023-04-23 20:57:06 -0400

    Add static hazard tile that damages / knocks back player

--- core/src/edu/cornell/gdiac/bubblegumbandit/helpers/Damage.java
+++ core/src/edu/cornell/gdiac/bubblegumbandit/helpers/Damage.java
@@ -2,6 +2,8 @@ package edu.cornell.gdiac.bubblegumbandit.helpers;
 
 public class Damage {
 
-    public static final float CRUSH_DAMAGE = 5f;
+    public static final float CRUSH_DAMAGE = 10f;
+    // TODO: Increase once player is invulnerable for short time period
+    public static final float HAZARD_DAMAGE = 2f;
 
 }
