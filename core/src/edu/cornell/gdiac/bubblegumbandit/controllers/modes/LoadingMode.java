0332eb2e4aa2a9efec3e52ef252183a553cfdf9a
Caroline <crh255@cornell.edu>
2023-05-08 16:28:36 -0400

    slowwww float on title

--- core/src/edu/cornell/gdiac/bubblegumbandit/controllers/modes/LoadingMode.java
+++ core/src/edu/cornell/gdiac/bubblegumbandit/controllers/modes/LoadingMode.java
@@ -141,6 +141,11 @@ public class LoadingMode implements Screen, InputProcessor, ControllerListener {
      */
     private final BitmapFont font;
 
+    /**
+     * The texture for the big ship illustration
+     */
+    private final Texture ship;
+
 
 
     /**
@@ -288,6 +293,8 @@ public class LoadingMode implements Screen, InputProcessor, ControllerListener {
      */
     private boolean active;
 
+    private float shipTime;
+
     /**
      * Returns the budget for the asset loader.
      * <p>
@@ -411,6 +418,8 @@ public class LoadingMode implements Screen, InputProcessor, ControllerListener {
          jet = internal.getEntry("jet", Texture.class);
 
          font = internal.getEntry("projectSpace", BitmapFont.class);
+         ship = internal.getEntry("bigShip", Texture.class);
+         ship.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 
         // Break up the status bar texture into regions
        /* statusBkgLeft = internal.getEntry("progress.backLeft", TextureRegion.class);
@@ -626,6 +635,13 @@ public class LoadingMode implements Screen, InputProcessor, ControllerListener {
                 );
             }
 
+           float offsetY = (float) (Math.sin(shipTime) * 10 )+20;
+
+            canvas.draw(ship, Color.WHITE, 0, 0,
+                canvas.getCamera().viewportWidth-ship.getWidth(),
+                canvas.getCamera().viewportHeight-ship.getHeight()+offsetY,
+                ship.getWidth(), ship.getHeight());
+
         }
         canvas.end();
     }
@@ -741,6 +757,8 @@ public class LoadingMode implements Screen, InputProcessor, ControllerListener {
             if (isReady() && listener != null) {
                 listener.exitScreen(this, 1);
             }
+
+            shipTime+=delta;
 
             if (isLevelSelect() && listener != null) {
                 listener.exitScreen(this, 6);
