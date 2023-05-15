9b023acd21f5ecffc422cfaebd8750494659246f
Ben Neuwirth <bmn36@cornell.edu>
2023-05-15 17:34:26 -0400

    Minimap shrinks on pause

--- core/src/edu/cornell/gdiac/bubblegumbandit/controllers/GameController.java
+++ core/src/edu/cornell/gdiac/bubblegumbandit/controllers/GameController.java
@@ -793,7 +793,7 @@ public class GameController implements Screen {
 
         }
         projectileController.update();
-        minimap.updateMinimap(dt, inputResults.didExpandMinimap());
+        minimap.updateMinimap(dt, inputResults.didExpandMinimap(), false);
         level.getAim().update(canvas, dt);
         laserController.updateLasers(dt, level.getWorld(), level.getBandit());
 
@@ -956,6 +956,7 @@ public class GameController implements Screen {
     public void pause() {
         // We need this method to stop all sounds when we pause.
         SoundController.pause();
+        minimap.updateMinimap(0, false, true);
         pauseScreen.show();
     }
 
